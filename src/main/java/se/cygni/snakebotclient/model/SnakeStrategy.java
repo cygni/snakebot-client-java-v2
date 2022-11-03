57b7d3fe62c84c329c196835d5737c129e9d35da
Benjamin Eriksen <Benja.e95@gmail.com>
2022-11-03 10:50:58 +0100

    extract future to Snakebot-class

--- src/main/java/se/cygni/snakebotclient/model/SnakeStrategy.java
+++ src/main/java/se/cygni/snakebotclient/model/SnakeStrategy.java
@@ -3,12 +3,9 @@ package se.cygni.snakebotclient.model;
 import se.cygni.snakebotclient.messages.GameMessage;
 import se.cygni.snakebotclient.model.enums.Direction;
 
-import java.util.concurrent.Future;
-
-
 public interface SnakeStrategy {
 
-      Future<Direction> getNextMove(GameState state);
+    Direction getNextMove(GameState state);
 
     void onMessage(GameMessage message);
 
